d1072d15b0b253dd2d27a6ba03633b69a5de0c50
Việt Bùi Xuân <vietbx23@gmail.com>
2025-09-20 14:56:28 +0000

    hiển thị dashboard admin bên trái menu danh mục, bên phải khung gian làm

---
+++ src/app/dashboard/admin/layout.tsx
@@ -0,0 +1,183 @@
+import Link from "next/link"
+import {
+  Bell,
+  CircleUser,
+  Home,
+  LineChart,
+  Menu,
+  Package,
+  Package2,
+  Search,
+  ShoppingCart,
+  Users,
+  Newspaper,
+  PenSquare,
+} from "lucide-react"
+
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const navItems = [
+      { href: "/dashboard/admin", icon: Home, label: "Tổng quan" },
+      { href: "#", icon: PenSquare, label: "Dự án", badge: "12" },
+      { href: "#", icon: Newspaper, label: "Bài viết" },
+      { href: "#", icon: Users, label: "Khách hàng" },
+      { href: "#", icon: LineChart, label: "Thống kê" },
+  ];
+  
+  return (
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div className="hidden border-r bg-muted/40 md:block">
+        <div className="flex h-full max-h-screen flex-col gap-2">
+          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+            <Link href="/" className="flex items-center gap-2 font-semibold">
+              <Package2 className="h-6 w-6 text-primary" />
+              <span className="">Kiến Trúc Xanh</span>
+            </Link>
+            <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
+              <Bell className="h-4 w-4" />
+              <span className="sr-only">Toggle notifications</span>
+            </Button>
+          </div>
+          <div className="flex-1">
+            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+              {navItems.map(item => (
+                 <Link
+                    key={item.label}
+                    href={item.href}
+                    className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                  >
+                    <item.icon className="h-4 w-4" />
+                    {item.label}
+                    {item.badge && <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">{item.badge}</Badge>}
+                  </Link>
+              ))}
+            </nav>
+          </div>
+          <div className="mt-auto p-4">
+            <Card>
+              <CardHeader className="p-2 pt-0 md:p-4">
+                <CardTitle>Cần Hỗ Trợ?</CardTitle>
+                <CardDescription>
+                  Liên hệ với đội ngũ phát triển nếu bạn gặp sự cố hoặc cần nâng cấp.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
+                <Button size="sm" className="w-full">
+                  Liên hệ
+                </Button>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="shrink-0 md:hidden"
+              >
+                <Menu className="h-5 w-5" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="flex flex-col">
+              <nav className="grid gap-2 text-lg font-medium">
+                <Link
+                  href="#"
+                  className="flex items-center gap-2 text-lg font-semibold mb-4"
+                >
+                  <Package2 className="h-6 w-6 text-primary" />
+                  <span className="sr-only">Kiến Trúc Xanh</span>
+                </Link>
+                {navItems.map(item => (
+                    <Link
+                      key={item.label}
+                      href={item.href}
+                      className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                    >
+                      <item.icon className="h-5 w-5" />
+                      {item.label}
+                       {item.badge && <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">{item.badge}</Badge>}
+                    </Link>
+                ))}
+              </nav>
+              <div className="mt-auto">
+                <Card>
+                  <CardHeader>
+                    <CardTitle>Cần Hỗ Trợ?</CardTitle>
+                    <CardDescription>
+                       Liên hệ với đội ngũ phát triển nếu bạn gặp sự cố hoặc cần nâng cấp.
+                    </CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <Button size="sm" className="w-full">
+                      Liên hệ
+                    </Button>
+                  </CardContent>
+                </Card>
+              </div>
+            </SheetContent>
+          </Sheet>
+
+          <div className="w-full flex-1">
+            <form>
+              <div className="relative">
+                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                <Input
+                  type="search"
+                  placeholder="Tìm kiếm chức năng..."
+                  className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
+                />
+              </div>
+            </form>
+          </div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full">
+                <CircleUser className="h-5 w-5" />
+                <span className="sr-only">Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Tài khoản</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Cài đặt</DropdownMenuItem>
+              <DropdownMenuItem>Hỗ trợ</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Đăng xuất</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </header>
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-white">
+            {children}
+        </main>
+      </div>
+    </div>
+  )
+}
--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -44,225 +44,221 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-import { Header } from "@/components/app/Header"
-import { Footer } from "@/components/app/Footer"
 
 
 export default function AdminDashboard() {
   return (
-    <div className="flex min-h-screen w-full flex-col bg-muted/40">
-       <Header />
-       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-        <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">
-                Tổng Doanh Thu
-              </CardTitle>
-              <DollarSign className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">45,231,890đ</div>
-              <p className="text-xs text-muted-foreground">
-                +20.1% so với tháng trước
-              </p>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">
-                Khách hàng mới
-              </CardTitle>
-              <Users className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">+235</div>
-              <p className="text-xs text-muted-foreground">
-                +180.1% so với tháng trước
-              </p>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">Dự án mới</CardTitle>
-              <CreditCard className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">+12</div>
-              <p className="text-xs text-muted-foreground">
-                +19% so với tháng trước
-              </p>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">
-                Liên hệ mới
-              </CardTitle>
-              <Activity className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">+57</div>
-              <p className="text-xs text-muted-foreground">
-                +20 since last hour
-              </p>
-            </CardContent>
-          </Card>
-        </div>
-        <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
-          <Card className="xl:col-span-2">
-            <CardHeader className="flex flex-row items-center">
-              <div className="grid gap-2">
-                <CardTitle>Khách hàng tiềm năng</CardTitle>
-                <CardDescription>
-                  Danh sách khách hàng gần đây đã liên hệ.
-                </CardDescription>
+    <>
+      <div className="flex items-center">
+          <h1 className="text-lg font-semibold md:text-2xl">Tổng quan</h1>
+      </div>
+      <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Tổng Doanh Thu
+            </CardTitle>
+            <DollarSign className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">45,231,890đ</div>
+            <p className="text-xs text-muted-foreground">
+              +20.1% so với tháng trước
+            </p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Khách hàng mới
+            </CardTitle>
+            <Users className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">+235</div>
+            <p className="text-xs text-muted-foreground">
+              +180.1% so với tháng trước
+            </p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Dự án mới</CardTitle>
+            <CreditCard className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">+12</div>
+            <p className="text-xs text-muted-foreground">
+              +19% so với tháng trước
+            </p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Liên hệ mới
+            </CardTitle>
+            <Activity className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">+57</div>
+            <p className="text-xs text-muted-foreground">
+              +20 since last hour
+            </p>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
+        <Card className="xl:col-span-2">
+          <CardHeader className="flex flex-row items-center">
+            <div className="grid gap-2">
+              <CardTitle>Khách hàng tiềm năng</CardTitle>
+              <CardDescription>
+                Danh sách khách hàng gần đây đã liên hệ.
+              </CardDescription>
+            </div>
+            <Button asChild size="sm" className="ml-auto gap-1">
+              <Link href="#">
+                Xem tất cả
+                <ArrowUpRight className="h-4 w-4" />
+              </Link>
+            </Button>
+          </CardHeader>
+          <CardContent>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Khách hàng</TableHead>
+                  <TableHead className="hidden xl:table-column">
+                    Dịch vụ quan tâm
+                  </TableHead>
+                  <TableHead className="hidden xl:table-column">
+                    Trạng thái
+                  </TableHead>
+                  <TableHead className="hidden md:table-cell lg:hidden xl:table-column">
+                    Ngày liên hệ
+                  </TableHead>
+                  <TableHead className="text-right">Số điện thoại</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    <div className="font-medium">Anh Trần Văn A</div>
+                    <div className="hidden text-sm text-muted-foreground md:inline">
+                      tran.a@email.com
+                    </div>
+                  </TableCell>
+                  <TableCell className="hidden xl:table-column">
+                    Hồ cá Koi
+                  </TableCell>
+                  <TableCell className="hidden xl:table-column">
+                    <Badge className="text-xs" variant="outline">
+                      Chờ tư vấn
+                    </Badge>
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
+                    2024-07-25
+                  </TableCell>
+                  <TableCell className="text-right">090xxxx123</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <div className="font-medium">Chị Nguyễn Thị B</div>
+                    <div className="hidden text-sm text-muted-foreground md:inline">
+                      nguyen.b@email.com
+                    </div>
+                  </TableCell>
+                  <TableCell className="hidden xl:table-column">
+                    Thiết kế sân vườn
+                  </TableCell>
+                  <TableCell className="hidden xl:table-column">
+                      <Badge className="text-xs" variant="secondary">
+                      Đã báo giá
+                    </Badge>
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
+                    2024-07-24
+                  </TableCell>
+                  <TableCell className="text-right">091xxxx456</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <div className="font-medium">Anh Lê Văn C</div>
+                    <div className="hidden text-sm text-muted-foreground md:inline">
+                      le.c@email.com
+                    </div>
+                  </TableCell>
+                  <TableCell className="hidden xl:table-column">
+                    Bảo trì cảnh quan
+                  </TableCell>
+                  <TableCell className="hidden xl:table-column">
+                    <Badge className="text-xs" variant="outline">
+                      Chờ tư vấn
+                    </Badge>
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
+                      2024-07-23
+                  </TableCell>
+                  <TableCell className="text-right">098xxxx789</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Dự án gần đây</CardTitle>
+          </CardHeader>
+          <CardContent className="grid gap-8">
+            <div className="flex items-center gap-4">
+              <Avatar className="hidden h-9 w-9 sm:flex">
+                <AvatarImage src="/images/gallery-1.jpg" alt="Avatar" />
+                <AvatarFallback>DA</AvatarFallback>
+              </Avatar>
+              <div className="grid gap-1">
+                <p className="text-sm font-medium leading-none">
+                  Biệt thự Quận 2
+                </p>
+                <p className="text-sm text-muted-foreground">
+                  Hoàn thành
+                </p>
               </div>
-              <Button asChild size="sm" className="ml-auto gap-1">
-                <Link href="#">
-                  Xem tất cả
-                  <ArrowUpRight className="h-4 w-4" />
-                </Link>
-              </Button>
-            </CardHeader>
-            <CardContent>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>Khách hàng</TableHead>
-                    <TableHead className="hidden xl:table-column">
-                      Dịch vụ quan tâm
-                    </TableHead>
-                    <TableHead className="hidden xl:table-column">
-                      Trạng thái
-                    </TableHead>
-                    <TableHead className="hidden md:table-cell lg:hidden xl:table-column">
-                      Ngày liên hệ
-                    </TableHead>
-                    <TableHead className="text-right">Số điện thoại</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  <TableRow>
-                    <TableCell>
-                      <div className="font-medium">Anh Trần Văn A</div>
-                      <div className="hidden text-sm text-muted-foreground md:inline">
-                        tran.a@email.com
-                      </div>
-                    </TableCell>
-                    <TableCell className="hidden xl:table-column">
-                      Hồ cá Koi
-                    </TableCell>
-                    <TableCell className="hidden xl:table-column">
-                      <Badge className="text-xs" variant="outline">
-                        Chờ tư vấn
-                      </Badge>
-                    </TableCell>
-                    <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                      2024-07-25
-                    </TableCell>
-                    <TableCell className="text-right">090xxxx123</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <div className="font-medium">Chị Nguyễn Thị B</div>
-                      <div className="hidden text-sm text-muted-foreground md:inline">
-                        nguyen.b@email.com
-                      </div>
-                    </TableCell>
-                    <TableCell className="hidden xl:table-column">
-                      Thiết kế sân vườn
-                    </TableCell>
-                    <TableCell className="hidden xl:table-column">
-                       <Badge className="text-xs" variant="secondary">
-                        Đã báo giá
-                      </Badge>
-                    </TableCell>
-                    <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                      2024-07-24
-                    </TableCell>
-                    <TableCell className="text-right">091xxxx456</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <div className="font-medium">Anh Lê Văn C</div>
-                      <div className="hidden text-sm text-muted-foreground md:inline">
-                        le.c@email.com
-                      </div>
-                    </TableCell>
-                    <TableCell className="hidden xl:table-column">
-                      Bảo trì cảnh quan
-                    </TableCell>
-                    <TableCell className="hidden xl:table-column">
-                      <Badge className="text-xs" variant="outline">
-                        Chờ tư vấn
-                      </Badge>
-                    </TableCell>
-                    <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                       2024-07-23
-                    </TableCell>
-                    <TableCell className="text-right">098xxxx789</TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle>Dự án gần đây</CardTitle>
-            </CardHeader>
-            <CardContent className="grid gap-8">
+              <div className="ml-auto font-medium">+150,000,000đ</div>
+            </div>
+            <div className="flex items-center gap-4">
+              <Avatar className="hidden h-9 w-9 sm:flex">
+                <AvatarImage src="/images/gallery-2.jpg" alt="Avatar" />
+                <AvatarFallback>DA</AvatarFallback>
+              </Avatar>
+              <div className="grid gap-1">
+                <p className="text-sm font-medium leading-none">
+                  Cafe Sân Vườn Gò Vấp
+                </p>
+                <p className="text-sm text-muted-foreground">
+                  Đang thi công
+                </p>
+              </div>
+              <div className="ml-auto font-medium">+250,000,000đ</div>
+            </div>
               <div className="flex items-center gap-4">
-                <Avatar className="hidden h-9 w-9 sm:flex">
-                  <AvatarImage src="/images/gallery-1.jpg" alt="Avatar" />
-                  <AvatarFallback>DA</AvatarFallback>
-                </Avatar>
-                <div className="grid gap-1">
-                  <p className="text-sm font-medium leading-none">
-                    Biệt thự Quận 2
-                  </p>
-                  <p className="text-sm text-muted-foreground">
-                    Hoàn thành
-                  </p>
-                </div>
-                <div className="ml-auto font-medium">+150,000,000đ</div>
+              <Avatar className="hidden h-9 w-9 sm:flex">
+                <AvatarImage src="/images/gallery-3.jpg" alt="Avatar" />
+                <AvatarFallback>DA</AvatarFallback>
+              </Avatar>
+              <div className="grid gap-1">
+                <p className="text-sm font-medium leading-none">
+                  Hồ Koi Bình Chánh
+                </p>
+                <p className="text-sm text-muted-foreground">
+                  Hoàn thành
+                </p>
               </div>
-              <div className="flex items-center gap-4">
-                <Avatar className="hidden h-9 w-9 sm:flex">
-                  <AvatarImage src="/images/gallery-2.jpg" alt="Avatar" />
-                  <AvatarFallback>DA</AvatarFallback>
-                </Avatar>
-                <div className="grid gap-1">
-                  <p className="text-sm font-medium leading-none">
-                    Cafe Sân Vườn Gò Vấp
-                  </p>
-                  <p className="text-sm text-muted-foreground">
-                    Đang thi công
-                  </p>
-                </div>
-                <div className="ml-auto font-medium">+250,000,000đ</div>
-              </div>
-               <div className="flex items-center gap-4">
-                <Avatar className="hidden h-9 w-9 sm:flex">
-                  <AvatarImage src="/images/gallery-3.jpg" alt="Avatar" />
-                  <AvatarFallback>DA</AvatarFallback>
-                </Avatar>
-                <div className="grid gap-1">
-                  <p className="text-sm font-medium leading-none">
-                    Hồ Koi Bình Chánh
-                  </p>
-                  <p className="text-sm text-muted-foreground">
-                    Hoàn thành
-                  </p>
-                </div>
-                <div className="ml-auto font-medium">+80,000,000đ</div>
-              </div>
-            </CardContent>
-          </Card>
-        </div>
-      </main>
-    </div>
+              <div className="ml-auto font-medium">+80,000,000đ</div>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   )
 }
-
-    
