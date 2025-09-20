701cbac8d4bf268146c06c1c435c685a8537b55b
Việt Bùi Xuân <vietbx23@gmail.com>
2025-09-20 12:24:01 +0000

    thiết kế giao diện trang chủ đẹp hơn,
    footer màu gradiunt cho đẹp nha

--- src/components/app/Footer.tsx
+++ src/components/app/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="bg-primary text-primary-foreground py-12">
+    <footer className="bg-gradient-footer text-primary-foreground py-12">
         <div className="container mx-auto px-4 grid grid-cols-1 md:grid-cols-4 gap-8">
           <div className="md:col-span-2">
             <h3 className="text-xl font-bold">Cảnh Quan Kiến Trúc Xanh</h3>
